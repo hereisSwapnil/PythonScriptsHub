9458278de45171026d2689b1ad46a6c89824f918
Parvez Khan <parvezkhan0802@gmail.com>
2023-10-14 00:50:10 +0530

    Add initial solution to the Knapsack Problem

---
+++ knapsack-problem-solution/knapsack-problem-solution.py
@@ -0,0 +1,30 @@
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    selected_items = []
+    i, w = n, capacity
+    while i > 0 and w > 0:
+        if dp[i][w] != dp[i - 1][w]:
+            selected_items.append(i - 1)
+            w -= weights[i - 1]
+        i -= 1
+
+    return dp[n][capacity], selected_items
+
+# Example usage:
+weights = [1, 3, 4, 5]
+values = [10, 40, 50, 70]
+capacity = 7
+max_value, selected_items = knapsack(weights, values, capacity)
+print(f"Maximum value: {max_value}")
+print(f"Selected items: {selected_items}")
